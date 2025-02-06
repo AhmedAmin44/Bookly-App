17e7412d847e4d61f168e5027c784e9a3416ef23
Ahmed Amin <119679806+AhmedAmin44@users.noreply.github.com>
2025-02-07 01:50:04 +0200

    add BestSellerListItem and install Font family

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/utils/images.dart';
 import 'package:app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:app/features/home/presentation/views/widgets/custom_list_view.dart';
 import 'package:app/features/home/presentation/views/widgets/custom_text_best_seller.dart';
@@ -13,8 +14,41 @@ class HomeViewBody extends StatelessWidget {
         CustomAppBar(),
         FeaturedBooksListView(),
         BestSeller(),
+        SizedBox(height: 40,),
+        BestSellerListItem()
       ],
     );
   }
 }
 
+class BestSellerListItem extends StatelessWidget {
+  const BestSellerListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      AppImages.testImage,
+                    ),
+                  )),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
+        ],
+      ),
+    );;
+  }
+}
